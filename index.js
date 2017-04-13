aa52805f4c7d764a4ea2e9574280500225bf285c
Klaus Nygård <klaus.nygard@gmail.com>
2017-04-13 10:23:20 +0300

    minor structure refactoring, introduce from-width, to-width, between-from and between-to, fix above and below to be exclusive

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ function plugin (opts) {
   var breakpoints = opts.breakpoints || {}
 
   return function (root, result) {
-    root.walkAtRules(/(above|between|below|breakpoint)/, function (rule) {
+    root.walkAtRules(/(above|from-width|below|to-width|between(-(from|to))?$|breakpoint)/, function (rule) {
       if (rule.name === 'breakpoint') {
         storeBreakpoint(rule.params)
         return rule.remove()
       }
-      transformRule(rule)
+      rule.params = transformRule(rule.name, getMeasures(rule.params))
       rule.name = 'media'
     })
   }
@@ -22,31 +22,50 @@ function plugin (opts) {
     breakpoints[params[0]] = params[1]
   }
 
-  function transformRule (rule) {
-    var units = getUnits(rule.params)
+  function formatQuery (queryRules) {
+    return ['screen'].concat(queryRules).join(' and ')
+  }
 
-    rule.params = 'screen and '
-
-    if (rule.name === 'above') {
-      rule.params += '(min-width: ' + units[0] + ')'
-    }
-
-    if (rule.name === 'below') {
-      rule.params += '(max-width: ' + units[0] + ')'
-    }
-
-    if (rule.name === 'between') {
-      rule.params += '(min-width: ' + units[0] + ') and '
-      rule.params += '(max-width: ' + units[1] + ')'
+  function transformRule (name, measures) {
+    switch (name) {
+      case 'above':
+        return formatQuery(['(min-width: ' + (measures[0] + 1) + 'px)'])
+      case 'from-width':
+        return formatQuery(['(min-width: ' + measures[0] + 'px)'])
+      case 'below':
+        return formatQuery(['(max-width: ' + (measures[0] - 1) + 'px)'])
+      case 'to-width':
+        return formatQuery(['(max-width: ' + measures[0] + 'px)'])
+      case 'between':
+        return formatQuery([
+          '(min-width: ' + (measures[0] + 1) + 'px)',
+          '(max-width: ' + (measures[1] - 1) + 'px)'
+        ])
+      case 'between-from':
+        return formatQuery([
+          '(min-width: ' + measures[0] + 'px)',
+          '(max-width: ' + (measures[1] - 1) + 'px)'
+        ])
+      case 'between-to':
+        return formatQuery([
+          '(min-width: ' + (measures[0] + 1) + 'px)',
+          '(max-width: ' + measures[1] + 'px)'
+        ])
     }
   }
 
-  function getUnits (params) {
+  function parseBreakpointUnit (unit) {
+    return typeof unit === 'string'
+      ? parseInt(unit.replace('px', ''))
+      : unit
+  }
+
+  function getMeasures (params) {
     return params.split(/\s/).map(function (param) {
       if (breakpoints.hasOwnProperty(param)) {
-        return breakpoints[param]
+        return parseBreakpointUnit(breakpoints[param])
       }
-      return param
+      return parseBreakpointUnit(param)
     })
   }
 }
